650dee75fbcf8461ea2163bccd12c45a26391b8e
GreyAxis One <ga.one@GreyAxiss-MacBook-Air.local>
2024-01-31 22:13:28 +0530

    added digigold partner

--- roles.go
+++ roles.go
@@ -5,6 +5,7 @@ type Role string
 const (
 	RoleAuthorizedPerson  Role = "authorized_person"
 	RoleReferralAssociate Role = "referral_associate"
+	RoleDigiGoldPartner   Role = "digi_gold_partner"
 	RoleCustomer          Role = "customer"
 	RoleAdmin             Role = "admin"
 	RoleDBOAdmin          Role = "dbo_admin"
