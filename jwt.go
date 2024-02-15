aa80ed0248d32d4b3022ae1a2e156a718bccdd5b
GreyAxis One <ga.one@GreyAxiss-MacBook-Air.local>
2024-02-15 14:49:54 +0530

    separate sign for admin

--- jwt.go
+++ jwt.go
@@ -22,6 +22,12 @@ type JWTClaimsDigiGoldPartner struct {
 	AgentID string `json:"agentID"`
 }
 
+type JWTClaimsAdmin struct {
+	jwt.RegisteredClaims
+	Role Role
+	ID   uint `json:"id"`
+}
+
 func Sign(claims *JWTClaims, secret []byte) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
@@ -75,6 +81,37 @@ func (claims *JWTClaimsDigiGoldPartner) Verify(tokenString string, secret []byte
 	if errWhileVerifying != nil {
 		return claims, errWhileVerifying
 	} else if claims, ok := token.Claims.(*JWTClaimsDigiGoldPartner); ok {
+		return claims, nil
+	} else {
+		// log.Fatal("unknown claims type, cannot proceed")
+
+		return claims, errors.New("unknown claims type, cannot proceed")
+	}
+}
+
+func (claims *JWTClaimsAdmin) Sign(secret []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString(secret)
+
+	fmt.Println(tokenString, err)
+
+	if err != nil {
+		return tokenString, err
+	}
+
+	return tokenString, nil
+}
+
+func (claims *JWTClaimsAdmin) Verify(tokenString string, secret []byte) (*JWTClaimsAdmin, error) {
+
+	token, errWhileVerifying := jwt.ParseWithClaims(tokenString, &JWTClaimsDigiGoldPartner{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+	if errWhileVerifying != nil {
+		return claims, errWhileVerifying
+	} else if claims, ok := token.Claims.(*JWTClaimsAdmin); ok {
 		return claims, nil
 	} else {
 		// log.Fatal("unknown claims type, cannot proceed")
