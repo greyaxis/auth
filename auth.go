aa5a19b48d10804299e1fc79005b2ab902017966
ga.one <web@greyaxis.io>
2024-04-03 15:54:48 +0530

    new function

--- auth.go
+++ auth.go
@@ -1,9 +1,11 @@
 package auth
 
 import (
+	"encoding/base64"
 	"log"
 	"strings"
 
+	"github.com/google/uuid"
 	"github.com/kataras/iris/v12"
 )
 
@@ -38,14 +40,16 @@ const (
 var (
 	JWT_SECRET       string
 	ADMIN_JWT_SECRET string
+	INTERSVC_API_KEY string
 )
 
-func Init(JWtSecret string, adminJWTSecret string) {
+func Init(JWtSecret string, adminJWTSecret string, interSVCAPIKey string) {
 	if JWtSecret == "" || adminJWTSecret == "" {
 		panic("empty values provided")
 	}
 	JWT_SECRET = JWtSecret
 	ADMIN_JWT_SECRET = adminJWTSecret
+	INTERSVC_API_KEY = interSVCAPIKey
 }
 
 func AuthenticateCustomer(ctx iris.Context) {
@@ -317,4 +321,68 @@ func AuthenticateDigitalBackOfficeUser(ctx iris.Context) {
 	ctx.Values().Set(RequestStateKey, reqState)
 	ctx.RegisterDependency(reqState)
 	ctx.Next()
+}
+
+type GatewayHeaders struct {
+	Authorization string    `header:"Authorization"`
+	ID            int       `header:"id"`
+	Role          string    `header:"role"`
+	AgentID       string    `header:"agendID"`
+	SessionID     uuid.UUID `header:"sessionID"`
+}
+
+var (
+	unauthorizedErr = authenticationError{
+		Error:   "Unauthorized",
+		Message: "access denied",
+	}
+	unknownHeaderTypeErr = authenticationError{
+		Error:   "This header type is not supported",
+		Message: "access denied",
+	}
+)
+
+func Authenticate(ctx iris.Context) {
+	var headers GatewayHeaders
+	err := ctx.ReadHeaders(&headers)
+	if err != nil {
+		ctx.StopWithProblem(iris.StatusUnauthorized, iris.NewProblem().
+			Key("error", unauthorizedErr))
+		return
+	}
+
+	parts := strings.Split(headers.Authorization, " ")
+	if len(parts) != 2 {
+		log.Println("token was splitted but the legnth is not enough, parts:", parts)
+		ctx.StopWithProblem(iris.StatusUnauthorized, iris.NewProblem().Key("error", unknownHeaderTypeErr))
+		return
+	}
+	if strings.ToLower(parts[0]) != "bearer" {
+		log.Println("token type was not bearer")
+		ctx.StopWithProblem(iris.StatusUnauthorized, iris.NewProblem().Key("error", unknownHeaderTypeErr))
+		return
+	}
+
+	token := parts[1]
+
+	byteToken, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		log.Println("byteToken, err:=base64.StdEncoding.DecodeString(token), err: ", err)
+		ctx.StopWithProblem(iris.StatusUnauthorized, iris.NewProblem().Key("error", unknownHeaderTypeErr))
+		return
+	}
+
+	if strings.Compare(string(byteToken), INTERSVC_API_KEY) != 0 {
+		ctx.StopWithProblem(iris.StatusUnauthorized, iris.NewProblem().Key("error", unauthorizedErr))
+		return
+	}
+
+	var state RequestState
+	state.ID = uint(headers.ID)
+	state.AgentID = headers.AgentID
+	state.SessionID = headers.SessionID
+	state.Role = Role(headers.Role)
+	ctx.Values().Set(RequestStateKey, state)
+	ctx.RegisterDependency(state)
+
 }
